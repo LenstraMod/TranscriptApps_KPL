41cd540140179a196c23fe88912dbe27e344431f
Faliqaqil <faliq.aqil.ra@gmail.com>
2026-06-19 01:01:50 +0700

    Transkrip GUI

---
+++ Frontend/Transkrip_UI.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Frontend
+{
+    partial class Transkrip_UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            txtDraft = new RichTextBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            txtFinal = new RichTextBox();
+            label2 = new Label();
+            buttonStop = new Button();
+            buttonBack = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Location = new Point(257, 520);
+            button1.Name = "button1";
+            button1.Size = new Size(280, 83);
+            button1.TabIndex = 0;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ButtonFace;
+            groupBox1.Controls.Add(txtDraft);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(0, 28);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(511, 444);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            // 
+            // txtDraft
+            // 
+            txtDraft.BorderStyle = BorderStyle.None;
+            txtDraft.Location = new Point(6, 42);
+            txtDraft.Name = "txtDraft";
+            txtDraft.ReadOnly = true;
+            txtDraft.Size = new Size(499, 382);
+            txtDraft.TabIndex = 1;
+            txtDraft.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(228, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Draft";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.ButtonFace;
+            groupBox2.Controls.Add(txtFinal);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Location = new Point(607, 28);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(541, 444);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            // 
+            // txtFinal
+            // 
+            txtFinal.BorderStyle = BorderStyle.None;
+            txtFinal.Location = new Point(9, 42);
+            txtFinal.Name = "txtFinal";
+            txtFinal.ReadOnly = true;
+            txtFinal.Size = new Size(526, 382);
+            txtFinal.TabIndex = 1;
+            txtFinal.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(251, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Final";
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(577, 520);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(280, 83);
+            buttonStop.TabIndex = 3;
+            buttonStop.Text = "Stop";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // buttonBack
+            // 
+            buttonBack.Location = new Point(22, 544);
+            buttonBack.Name = "buttonBack";
+            buttonBack.Size = new Size(112, 34);
+            buttonBack.TabIndex = 4;
+            buttonBack.Text = "back";
+            buttonBack.UseVisualStyleBackColor = true;
+            buttonBack.Click += buttonBack_Click;
+            // 
+            // Transkrip_UI
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(1148, 625);
+            Controls.Add(buttonBack);
+            Controls.Add(buttonStop);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Name = "Transkrip_UI";
+            Text = "Transkrip_UI";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private Label label2;
+        private RichTextBox txtDraft;
+        private RichTextBox txtFinal;
+        private Button buttonStop;
+        private Button buttonBack;
+    }
+}
---
+++ Frontend/Transkrip_UI.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Frontend
+{
+    public partial class Transkrip_UI : Form
+    {
+        // 1. Data dummy transkrip
+        private string[] kataDraft = {
+            "Halo selamat pagi semuanya, ",
+            "hari ini kita akan membahas mengenai progres ",
+            "pengembangan aplikasi transkrip berbasis GUI. ",
+            "Target kita minggu ini adalah menyelesaikan frontend ",
+            "dan mengintegrasikannya dengan backend sistem."
+        };
+
+        private string teksFinal = "Inti Rapat: Pembahasan progres aplikasi transkrip GUI. Target minggu ini menyelesaikan frontend dan integrasi backend.";
+
+        private int indeksKata = 0;
+        private System.Windows.Forms.Timer timerTranskrip;
+
+        public Transkrip_UI()
+        {
+            InitializeComponent();
+            SetupTimer();
+        }
+
+        // 2. Inisialisasi Timer
+        private void SetupTimer()
+        {
+            timerTranskrip = new System.Windows.Forms.Timer();
+            timerTranskrip.Interval = 1500;
+            timerTranskrip.Tick += TimerTranskrip_Tick;
+        }
+
+        // 3. Fungsi saat tombol START diklik
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDraft.Clear();
+            txtFinal.Clear();
+            indeksKata = 0;
+            timerTranskrip.Start();
+        }
+
+        // 4. Fungsi saat tombol STOP diklik
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (timerTranskrip.Enabled)
+            {
+                timerTranskrip.Stop();
+                txtFinal.Text = teksFinal;
+            }
+        }
+
+        // 5. Fungsi BARU saat tombol BACK diklik
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            // Pastikan timer berhenti jika masih berjalan sebelum pindah halaman
+            timerTranskrip.Stop();
+
+            // Membuat instansiasi objek halaman HomePage baru
+            HomePage halamanHome = new HomePage();
+
+            // Memunculkan halaman HomePage
+            halamanHome.Show();
+
+            // Menutup/menyembunyikan halaman Transkrip_UI saat ini
+            this.Close();
+        }
+
+        // 6. Efek simulasi real-time mengetik kata demi kata
+        private void TimerTranskrip_Tick(object sender, EventArgs e)
+        {
+            if (indeksKata < kataDraft.Length)
+            {
+                txtDraft.AppendText(kataDraft[indeksKata]);
+                indeksKata++;
+            }
+            else
+            {
+                timerTranskrip.Stop();
+                txtFinal.Text = teksFinal;
+            }
+        }
+    }
+}
